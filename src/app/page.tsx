d43a3650588839e41de60860ee9a5dbdc86cf080
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-11-23 21:50:11 +0200

    Added RainbowKit to connect wallet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
 "use client";
 
 import Link from "next/link";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
-  return <Link href="/accounts/new">New Account</Link>;
+  return (
+    <>
+      <Link href="/accounts/new">New Account</Link>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          padding: 12,
+        }}
+      >
+        <ConnectButton />
+      </div>
+    </>
+  );
 }
---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = "Portfolio Management";
+export const REOWN_PROJECT_ID: string =
+  process.env.NEXT_PUBLIC_REOWN_PROJECT_ID || "";
